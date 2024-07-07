e5f02fa488bea3afa42af9241649cbd926bb01db
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-07-07 16:11:54 +0200

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+const tailwind = {
+  mode: "jit",
+  content: {
+    files: ["src/**/*.rs", "index.html"],
+  },
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default tailwind;

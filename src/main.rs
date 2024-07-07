be5f4083b7d98eb5cb0c9a24cb6bf1fe094f35b6
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-07-07 18:12:35 +0200

    removed unnecessary files

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
+use pages::home::Home;
+use pages::page_not_found::PageNotFound;
+use yew::html::Scope;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 mod components;
-mod content;
-mod generator;
 mod pages;
-use pages::author::Author;
-use pages::author_list::AuthorList;
-use pages::home::Home;
-use pages::page_not_found::PageNotFound;
-use pages::post::Post;
-use pages::post_list::PostList;
-use yew::html::Scope;
 
 #[derive(Routable, PartialEq, Eq, Clone, Debug)]
 pub enum Route {
-    #[at("/posts/:id")]
-    Post { id: u64 },
-    #[at("/posts")]
-    Posts,
-    #[at("/authors/:id")]
-    Author { id: u64 },
-    #[at("/authors")]
-    Authors,
     #[at("/")]
     Home,
     #[not_found]
@@ -37,6 +23,7 @@ pub enum Msg {
 pub struct App {
     navbar_active: bool,
 }
+
 impl Component for App {
     type Message = Msg;
     type Properties = ();
@@ -66,18 +53,19 @@ impl Component for App {
                 </main>
                 <footer class="footer">
                     <div class="content has-text-centered">
-                        { "Powered by " }
+                    <p>{ "Powered by " }
                         <a href="https://yew.rs">{ "Yew" }</a>
                         { " using " }
                         <a href="https://bulma.io">{ "Bulma" }</a>
                         { " and images from " }
-                        <a href="https://unsplash.com">{ "Unsplash" }</a>
+                        <a href="https://unsplash.com">{ "Unsplash" }</a></p>
                     </div>
                 </footer>
             </BrowserRouter>
         }
     }
 }
+
 impl App {
     fn view_nav(&self, link: &Scope<Self>) -> Html {
         let Self { navbar_active, .. } = *self;
@@ -85,59 +73,31 @@ impl App {
         let active_class = if !navbar_active { "is-active" } else { "" };
 
         html! {
-            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
-
-                    <button class={classes!("navbar-burger", "burger", active_class)}
-                        aria-label="menu" aria-expanded="false"
-                        onclick={link.callback(|_| Msg::ToggleNavbar)}
-                    >
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-                <div class={classes!("navbar-menu", active_class)}>
-                    <div class="navbar-start">
-                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
-                            { "Home" }
-                        </Link<Route>>
-                        <Link<Route> classes={classes!("navbar-item")} to={Route::Posts}>
-                            { "Posts" }
-                        </Link<Route>>
-
-                        <div class="navbar-item has-dropdown is-hoverable">
-                            <div class="navbar-link">
-                                { "More" }
-                            </div>
-                            <div class="navbar-dropdown">
-                                <Link<Route> classes={classes!("navbar-item")} to={Route::Authors}>
-                                    { "Meet the authors" }
-                                </Link<Route>>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        }
+                    <div class="navbar bg-base-100">
+          <div class="flex-1">
+            <a class="btn btn-ghost text-xl">{"daisyUI"}</a>
+          </div>
+          <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+              <li><a>{"Link"}</a></li>
+              <li>
+                <details>
+                  <summary>{"Parent"}</summary>
+                  <ul class="bg-base-100 rounded-t-none p-2">
+                    <li><a>{"Link 1"}</a></li>
+                    <li><a>{"Link 2"}</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </div>
+                }
     }
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
-        Route::Post { id } => {
-            html! { <Post seed={id} /> }
-        }
-        Route::Posts => {
-            html! { <PostList /> }
-        }
-        Route::Author { id } => {
-            html! { <Author seed={id} /> }
-        }
-        Route::Authors => {
-            html! { <AuthorList /> }
-        }
         Route::Home => {
             html! { <Home /> }
         }
@@ -148,6 +108,5 @@ fn switch(routes: Route) -> Html {
 }
 
 fn main() {
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     yew::Renderer::<App>::new().render();
 }
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -11,32 +11,34 @@ impl Component for Home {
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div class="tile is-ancestor is-vertical">
-                <div class="tile is-child hero">
-                    <div class="hero-body container pb-0">
-                        <h1 class="title is-1">{ "Welcome..." }</h1>
-                        <h2 class="subtitle">{ "...to the best yew content" }</h2>
-                    </div>
-                </div>
-
-                <div class="tile is-child">
-                    <figure class="image is-3by1">
-                        <img alt="A random image for the input term 'yew'." src="https://source.unsplash.com/random/1200x400/?yew" />
-                    </figure>
-                </div>
-
-                <div class="tile is-parent container">
-                    { self.view_info_tiles() }
-                </div>
+        <div class="">
+          <div class="">
+            <div class="">
+              <h1 class="">{ "Welcome..." }</h1>
+              <h2 class="">{ "...to the best yew content" }</h2>
             </div>
+          </div>
+
+          <div class="">
+            <figure class="">
+              <img
+                alt="A random image for the input term 'yew'."
+                src="https://source.unsplash.com/random/1200x400/?yew"
+              />
+            </figure>
+          </div>
+
+          <div class="">{ self.view_info_tiles() }</div>
+        </div>
         }
     }
 }
+
 impl Home {
     fn view_info_tiles(&self) -> Html {
         html! {
             <>
-                <div class="tile is-parent">
+                <div class="bg-red-500">
                     <div class="tile is-child box">
                         <p class="title">{ "What are yews?" }</p>
                         <p class="subtitle">{ "Everything you need to know!" }</p>
